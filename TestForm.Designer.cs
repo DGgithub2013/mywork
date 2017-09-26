f2e0ea1fadcebbecc47abab04fecfc1ed5bf3bf0
DGgithub2013 <dganea@yahoo.com>
2017-09-26 08:50:41 -0700

    Add files via upload

---
+++ TestForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace rssFeed
+{
+    partial class TestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.btnGetFeed = new System.Windows.Forms.Button();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(15, 12);
+            this.txtTitle.Multiline = true;
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtTitle.Size = new System.Drawing.Size(834, 257);
+            this.txtTitle.TabIndex = 0;
+            // 
+            // btnGetFeed
+            // 
+            this.btnGetFeed.Location = new System.Drawing.Point(258, 288);
+            this.btnGetFeed.Name = "btnGetFeed";
+            this.btnGetFeed.Size = new System.Drawing.Size(144, 23);
+            this.btnGetFeed.TabIndex = 1;
+            this.btnGetFeed.Text = " First get rss feed";
+            this.btnGetFeed.UseVisualStyleBackColor = true;
+            this.btnGetFeed.Click += new System.EventHandler(this.btnGetFeed_Click);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(430, 288);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(135, 23);
+            this.btnConvert.TabIndex = 2;
+            this.btnConvert.Text = "Then convert to ical";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // TestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(861, 340);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.btnGetFeed);
+            this.Controls.Add(this.txtTitle);
+            this.Name = "TestForm";
+            this.Text = " Test Display";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Button btnGetFeed;
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
+
---
+++ TestForm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using RSSReader;
+using VCalendarConverter;
+using rssFeed.Exporter;
+namespace rssFeed
+{
+    public partial class TestForm : Form
+    {
+        public TestForm()
+        {
+            InitializeComponent();
+        }
+           private void btnConvert_Click(object sender, EventArgs e)
+       
+        {
+            var ec = new Exporter.ExporterController("C:\\Temp\\eventSetting.xml");
+          
+            string url = "http://umbraco_cms.vancouver.ca/rss";
+          // var readableRss = covertRss(url);
+         var icals = ec.ConvertiCal(url);
+            var icalcontroller = new VCalendarControler();
+            var radeableical = icalcontroller.GetICalendar(icals.VCalendarItemList[0]);
+           // txtTitle.Text = readableRss;
+           txtTitle.Text = radeableical;
+        }
+        private string covertRss(string url)
+        {
+            var s = RssReader.Read(url);
+            StringBuilder sb = new StringBuilder();
+            foreach (RssItem rs in s)
+            {
+                sb.AppendLine(rs.Title);
+                sb.AppendLine(rs.StartDate.ToString());
+                sb.AppendLine(rs.EndDate.ToString());
+                sb.AppendLine(rs.Link);
+                sb.AppendLine(rs.Description);
+                sb.AppendLine(rs.Location);
+                sb.AppendLine(rs.Subject);
+                sb.AppendLine(rs.PublicationDate);
+                sb.AppendLine(rs.TimeZoneAdjustment.ToString());
+                sb.AppendLine(rs.GUID.ToString());
+            }
+
+            return sb.ToString();
+        }
+
+        private void btnGetFeed_Click(object sender, EventArgs e)
+        {
+            var ec = new Exporter.ExporterController("C:\\Temp\\eventSetting.xml");
+
+            string url = "http://umbraco_cms.vancouver.ca/rss";
+             var readableRss = covertRss(url);
+          
+            txtTitle.Text = readableRss;
+          
+        }
+
+   
+
+    }
+
+
+
+
+
+
+}
